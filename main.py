1ec52354a9f175819f33da2a645a04facba2b4ea
xborismenx <boris.tikhonov.21@gmail.com>
2026-06-09 17:50:41 +0300

    Add users and admin routers to the FastAPI application

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from src.routers import auth_router
+from src.routers import auth_router, users_router, admin_router
 
 app = FastAPI()
 
 prefix = "/api/v1"
 
 app.include_router(router=auth_router, prefix=f"{prefix}/auth", tags=["Authentication"])
+app.include_router(router=users_router, prefix=f"{prefix}/users", tags=["users"])
+app.include_router(router=admin_router, prefix=f"{prefix}/admin", tags=["Admin Panel"])
