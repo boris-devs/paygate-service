eba4997c5273f0cada444495c2a952c7aa0d81b5
xborismenx <boris.tikhonov.21@gmail.com>
2026-06-09 17:44:49 +0300

    Add user management methods to UserRepository and admin role validation

--- src/security/utils.py
+++ src/security/utils.py
@@ -3,6 +3,7 @@ from fastapi import status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from models.users import UserRole, User
 from repositories.users import UserRepository
 from src.security.token_manager import decode_access_token
 from src.db import get_db
@@ -50,3 +51,12 @@ async def get_current_user(
 		raise credentials_exception
 
 	return user
+
+
+async def get_current_admin(current_user: User = Depends(get_current_user)) -> User:
+	if current_user.role != UserRole.ADMIN:
+		raise HTTPException(
+			status_code=status.HTTP_403_FORBIDDEN,
+			detail="You do not have permission to perform this action"
+		)
+	return current_user
